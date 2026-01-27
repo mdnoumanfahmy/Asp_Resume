8b6b8f97de433b44a59c52fbbef1b60272c54f30
Md Nouman Fahmy <mdnoumanf@gmail.com>
2026-01-27 22:25:23 +0530

    added Resume app

---
+++ WebApplication2_Resume/Controllers/User.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2_Resume.Controllers
+{
+    public class User : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Experience()
+        {
+            return View();
+        }
+
+        public IActionResult Hobbies()
+        {
+            return View();
+        }
+    }
+}
