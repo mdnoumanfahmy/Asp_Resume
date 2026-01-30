bfc0e7d6f153f9b7f9541dd52d1583da8080c70e
Md Rehan <mdrehan4all@gmail.com>
2026-01-30 15:07:58 +0530

    added about page

--- WebApplication2_Resume/obj/Debug/net9.0/WebApplication2_Resume.AssemblyInfo.cs
+++ WebApplication2_Resume/obj/Debug/net9.0/WebApplication2_Resume.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2_Resume")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b6b8f97de433b44a59c52fbbef1b60272c54f30")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2_Resume")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2_Resume")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
